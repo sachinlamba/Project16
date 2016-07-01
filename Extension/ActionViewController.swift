edb27d6c1144e9434f793a452a7f4ef6d6a8de5a
sachin lamba <sachin.lamba@moldedbits.com>
2016-07-01 12:33:37 +0530

    Extension start preparation for run on web pages.

---
+++ Extension/ActionViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Sachin Lamba on 30/06/16.
+//  Copyright © 2016 LambaG. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        if let inputItem = extensionContext!.inputItems as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
+                itemProvider.loadItemForTypeIdentifier(kUTTypePropertyList as String, options: nil)
+                        { [unowned self] (dict, error) in
+                    
+                    }
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequestReturningItems(self.extensionContext!.inputItems, completionHandler: nil)
+    }
+
+}
