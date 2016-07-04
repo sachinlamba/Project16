9f86170a10136ccac2df6812b5a2bd871f5565c8
sachin lamba <sachin.lamba@moldedbits.com>
2016-07-04 14:56:15 +0530

    Alert msg for title.

--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -19,46 +19,41 @@ class ActionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: #selector(done))
+        
         let notificationCenter = NSNotificationCenter.defaultCenter()
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIKeyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIKeyboardWillChangeFrameNotification, object: nil)
         
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: #selector(done))
-        
         if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
-          if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
-              itemProvider.loadItemForTypeIdentifier(kUTTypePropertyList as String, options: nil) { [unowned self] (dict, error) in
-                            let itemDictionary = dict as! NSDictionary
-                            let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
-                            //print(javaScriptValues)
-                            self.pageTitle = javaScriptValues["title"] as! String
-                            self.pageURL = javaScriptValues["URL"] as! String
-                
-                            dispatch_async(dispatch_get_main_queue()) {
-                                self.title = self.pageTitle
-                        }
-               }
+            if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
+                itemProvider.loadItemForTypeIdentifier(kUTTypePropertyList as String, options: nil) { [unowned self] (dict, error) in
+                    let itemDictionary = dict as! NSDictionary
+                    let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
+                    
+                    self.pageTitle = javaScriptValues["title"] as! String
+                    self.pageURL = javaScriptValues["URL"] as! String
+                    
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.title = self.pageTitle
+                    }
+                }
             }
         }
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
         let item = NSExtensionItem()
         let webDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: ["customJavaScript": script.text]]
         let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
         item.attachments = [customJavaScript]
         
-        
-        self.extensionContext!.completeRequestReturningItems(self.extensionContext!.inputItems, completionHandler: nil)
+        extensionContext!.completeRequestReturningItems([item], completionHandler: nil)
     }
     
     func adjustForKeyboard(notification: NSNotification) {
@@ -78,9 +73,4 @@ class ActionViewController: UIViewController {
         let selectedRange = script.selectedRange
         script.scrollRangeToVisible(selectedRange)
     }
-
-    
-    
-    
-    
 }
